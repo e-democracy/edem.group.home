2da40837db0c723917e2011b20ad55e69f089fe7
Bill Bushey <wbushey@gmail.com>
2013-04-16 20:55:17 -0500

    Added isPublic/isPrivate properties to UsBar

--- edem/group/home/usbar.py
+++ edem/group/home/usbar.py
@@ -2,15 +2,15 @@
 from zope.component import createObject
 from zope.contentprovider.interfaces import UpdateNotCalled
 from zope.app.pagetemplate import ViewPageTemplateFile
-from Products.GSGroupMember.groupmembership import user_member_of_group
-from gs.group.base.contentprovider import GroupContentProvider
 from gs.group.member.list.queries import MembersQuery
-from Products.GSGroupMember.groupmembership import get_group_userids
-from Products.CustomUserFolder.CustomUser import user_image_path
+from gs.group.base.contentprovider import GroupContentProvider
 from gs.group.base.interfaces import IGSGroupMarker
 from gs.group.base.page import GroupPage
+from gs.group.privacy.interfaces import IGSGroupVisibility
 from gs.viewlet import SiteContentProvider, WeightOrderedViewletManager
-
+from Products.CustomUserFolder.CustomUser import user_image_path
+from Products.GSGroupMember.groupmembership import get_group_userids,\
+                                                    user_member_of_group
 import random
 
 USBARLIMIT = 5
@@ -61,6 +61,20 @@ class UsBar(GroupContentProvider):
             self.__isMember = user_member_of_group(self.userInfo, 
                                                     self.context)
         return self.__isMember
+
+    @Lazy
+    def isPrivate(self):
+        ''' Indicates if the group the UsBar is being displayed in is
+        Private'''
+        vis = IGSGroupVisibility(self.groupInfo)
+        return vis.isPrivate
+
+
+    @Lazy
+    def isPublic(self):
+        ''' Indicates if the group the UsBar is being displayed in is Public'''
+        vis = IGSGroupVisibility(self.groupInfo)
+        return vis.isPublic
         
     def update(self):
         self.__updated = True
